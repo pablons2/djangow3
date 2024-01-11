676a9c5d8e87d608f51c5167fa30ababc8ecc613
pablo <pablo.santos@gstecnologia-al.com.br>
2024-01-11 16:32:21 -0300

    finish | Django Syntax

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -23,9 +23,9 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
-  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
-    'mymembers': mymembers,
+    'x': ['Apple', 'Banana', 'Cherry'], 
+    'y': ['Apple', 'Banana', 'Cherry'], 
   }
-  return HttpResponse(template.render(context, request))        
+  return HttpResponse(template.render(context, request))  
